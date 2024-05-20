9cb6017afa65adb6e30e2fd87f3874b7604794bc
Lucas Nunes <dev.lucasnunes@gmail.com>
2024-05-20 16:28:45 -0300

    validations

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -7,6 +7,13 @@ class Supplier < ApplicationRecord
   validates :contact, presence: true
   validates :cnpj, presence: true
 
+  include PgSearch::Model
+  pg_search_scope :search_by_name, against: :name,
+                  using: {
+                    tsearch: { prefix: true },
+                    trigram: {threshold: 0.1}
+                  }
+
   validate :cnpj_validate
 
   private
